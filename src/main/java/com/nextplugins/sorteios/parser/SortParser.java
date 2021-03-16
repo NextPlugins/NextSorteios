4ec5a56055941e07e047ecc3d072461b9867801e
Yuhtin <rio.daviduarte@gmail.com>
2021-03-16 10:44:38 -0300

    feat(parser): sort parser

--- src/main/java/com/nextplugins/sorteios/parser/SortParser.java
+++ src/main/java/com/nextplugins/sorteios/parser/SortParser.java
@@ -1,7 +1,12 @@
 package com.nextplugins.sorteios.parser;
 
 import com.nextplugins.sorteios.api.Sort;
+import com.nextplugins.sorteios.utils.ColorUtils;
 import org.bukkit.configuration.ConfigurationSection;
+
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author Yuhtin
@@ -10,10 +15,21 @@ import org.bukkit.configuration.ConfigurationSection;
 
 public class SortParser {
 
+    public static Set<Sort> parseListSection(ConfigurationSection section) {
+
+        return section.getKeys(false)
+                .stream()
+                .map(section::getConfigurationSection)
+                .filter(Objects::nonNull)
+                .map(SortParser::parseSection)
+                .collect(Collectors.toSet());
+
+    }
+    
     public static Sort parseSection(ConfigurationSection section) {
 
         return Sort.builder()
-                .coloredName(section.getString("coloredName"))
+                .coloredName(ColorUtils.colored(section.getString("coloredName")))
                 .commands(section.getStringList("commands"))
                 .build();
 
